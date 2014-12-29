ed739806dfe91549b57ce2aa831419127247c955
Michael Hunter <hunter.michael.k@gmail.com>
2014-12-29 18:21:58 +0800

    First commit.

---
+++ MarcoPolloCampero/MarcoPolloCampero/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MarcoPolloCampero
+//
+//  Created by Michael Hunter on 12/29/14.
+//  Copyright (c) 2014 Michael Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)postItButtonPressed:(id)sender;
+
+@end
+
---
+++ MarcoPolloCampero/MarcoPolloCampero/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  MarcoPolloCampero
+//
+//  Created by Michael Hunter on 12/29/14.
+//  Copyright (c) 2014 Michael Hunter. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)postItButtonPressed:(id)sender {
+    NSLog(@"Post It button was pressed!");
+}
+@end
