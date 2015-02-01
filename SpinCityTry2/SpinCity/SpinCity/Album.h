f4b27e8ea7cd44c80c5471a02c73483697ee3561
Michael Hunter <hunter.michael.k@gmail.com>
2015-02-01 12:48:25 -0500

    Created the Album class and the DAO.

--- SpinCityTry2/SpinCity/SpinCity/Album.h
+++ SpinCityTry2/SpinCity/SpinCity/Album.h
@@ -10,4 +10,15 @@
 
 @interface Album : NSObject
 
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *artist;
+@property (nonatomic, copy) NSString *summary;
+@property (nonatomic, copy) NSString *locationInStore;
+
+@property (nonatomic, assign) float price;
+
+-(id)initWithTitle:(NSString *)title artist:(NSString *)artist
+           summary:(NSString*)summary price:(float)price
+   locationInStore:(NSString*)locationInStore;
+
 @end
---
+++ SpinCityTry2/SpinCity/SpinCity/AlbumDataController.h
@@ -0,0 +1,26 @@
+//
+//  AlbumDataController.h
+//  SpinCity
+//
+//  Created by Michael Hunter on 2/1/15.
+//  Copyright (c) 2015 Michael Hunter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Album;
+
+@interface AlbumDataController : NSObject
+
+- (NSUInteger) albumCount;
+
+- (Album *)albumAtIndex:(NSUInteger)index;
+
+-(void) addAlbumWithTitle:(NSString*)title
+                   artist:(NSString *)artist
+                  summary:(NSString *)summary
+                    price:(float)price
+          locationInStore:(NSString *)locationInStore;
+
+
+@end
