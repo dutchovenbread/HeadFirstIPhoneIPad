e0775d29e1cc0b08a5984f63dd77ae0b952af0db
Michael Hunter <hunter.michael.k@gmail.com>
2015-02-01 12:10:10 -0500

    Added blank Album class.

---
+++ SpinCityTry2/SpinCity/SpinCity/Album.h
@@ -0,0 +1,13 @@
+//
+//  Album.h
+//  SpinCity
+//
+//  Created by Michael Hunter on 2/1/15.
+//  Copyright (c) 2015 Michael Hunter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Album : NSObject
+
+@end
---
+++ SpinCityTry2/SpinCity/SpinCity/Album.m
@@ -0,0 +1,13 @@
+//
+//  Album.m
+//  SpinCity
+//
+//  Created by Michael Hunter on 2/1/15.
+//  Copyright (c) 2015 Michael Hunter. All rights reserved.
+//
+
+#import "Album.h"
+
+@implementation Album
+
+@end
